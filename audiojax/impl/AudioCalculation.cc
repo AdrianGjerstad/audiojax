0bbf2e80b5e469c817570971618d75342cf8e6cc
AdrianGjerstad <48812634+AdrianGjerstad@users.noreply.github.com>
2021-02-06 21:58:06 -0500

    Implement other 3 functions

--- audiojax/impl/AudioCalculation.cc
+++ audiojax/impl/AudioCalculation.cc
@@ -7,3 +7,34 @@ double jax::calc::pitch2frequency(double pitch) {
   return std::pow(2, (pitch-69)/12) * 440;
 }
 
+double jax::calc::frequency2pitch(double frequency) {
+  // Frequency not allowed to be zero or negative
+  if(frequency <= 0) return 0;
+
+  // f(x) = 12*log2(x/440) + 69
+  return std::log2(frequency/440)*12 + 69;
+}
+
+double jax::calc::velocity2gain(double velocity) {
+  // Velocity not allowed to be negative, but zero velocity
+  // also results in 0 gain, so whatever.
+  if(velocity <= 0) return 0;
+
+  // f(x) = 2^((20*log10(x/127))/3)
+  return std::pow(2, (std::log10(velocity/127)*20)/3);
+}
+
+double jax::calc::gain2velocity(double gain) {
+  // Gain not allowed to be negative, and zero gain also
+  // maps to zero velocity.
+  if(gain <= 0) return 0;
+
+  // If I really wanted to be MIDI compliant, I would also
+  // add a check to make sure gain wasn't larger than 127,
+  // but I'm also using doubles for all of this, so I think
+  // I don't need to worry about it.
+  
+  // f(x) = 10^((3*log2(x))/40) * 127
+  return std::pow(10, (std::log2(gain)*3)/40) * 127;
+}
+
